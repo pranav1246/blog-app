70cd00b72b8f361e5cd75d85a5aaa33b11cb3b92
Pranav <responsetopranav@gmail.com>
2024-08-26 14:04:55 +0530

    final

--- Server/front-end/src/App.js
+++ Server/front-end/src/App.js
@@ -1,39 +1,35 @@
-
 import './App.css';
 import Header from './components/Header';
-// import Sidebar from './components/SideNav';
 import AuthForm from './components/account/Login';
 import DataProvider from './context/DataProvider';
 import HomePage from './components/Home';
 import CreatePost from './components/CreatePost';
 import UpdatePost from './components/UpdatePost';
 import Footer from './components/Footer';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-     <header>
-    <Header />
-    </header>
-    <main>
-    <DataProvider >
-   
-    <div className="content">
-    <Routes>
-    <Route path='/auth' element={<AuthForm />} />
-    <Route path='/' element={<HomePage />} />
-    <Route path='/create' element={<CreatePost />} />
-    <Route path='/update/:id' element={<UpdatePost />} />
-     </Routes>
-      </div>
-    </DataProvider>
-  
-     </main>
-     <footer>
-      <Footer />
-    </footer>
-     </BrowserRouter>
+      <DataProvider>
+        <header>
+          <Header />
+        </header>
+        <main>
+          <div className="content">
+            <Routes>
+              <Route path='/auth' element={<AuthForm />} />
+              <Route path='/' element={<HomePage />} />
+              <Route path='/create' element={<CreatePost />} />
+              <Route path='/update/:id' element={<UpdatePost />} />
+            </Routes>
+          </div>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </DataProvider>
+    </BrowserRouter>
   );
 }
 
